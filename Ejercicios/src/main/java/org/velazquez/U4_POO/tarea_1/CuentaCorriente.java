fcd8058398c4561b046a8fb4482aa98ac2db5f54
DrianGM <adriangm8110@gmail.com>
2024-01-08 10:30:50 +0100

    Tema 4 Iniciado

---
+++ Ejercicios/src/main/java/org/velazquez/U4_POO/tarea_1/CuentaCorriente.java
@@ -0,0 +1,10 @@
+package org.velazquez.U4_POO.tarea_1;
+
+public class CuentaCorriente {
+    public static void main(String[] args) {
+        String nombre = "";
+        int dni = 0;
+        long saldo = 0;
+        long limit_descub = 0;
+    }
+}
