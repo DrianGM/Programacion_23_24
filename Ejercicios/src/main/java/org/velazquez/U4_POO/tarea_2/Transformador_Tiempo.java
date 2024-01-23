f9726381fb5d9517ed74285ddbc195bb7e825180
DrianGM <adriangm8110@gmail.com>
2024-01-23 09:40:06 +0100

    Ejercicio 10 Avanzado

---
+++ Ejercicios/src/main/java/org/velazquez/U4_POO/tarea_2/Transformador_Tiempo.java
@@ -0,0 +1,36 @@
+package org.velazquez.U4_POO.tarea_2;
+
+import java.util.Scanner;
+
+public class Transformador_Tiempo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int segundos = 0;
+        int minutos = 0;
+        int horas = 0;
+        int cociente = 0;
+        int cociente2 = 0;
+        int resto = 0;
+        int n = sc.nextInt();
+        while(true){
+            if (segundos+n<=59){
+                segundos=segundos+n;
+            } else {
+                cociente=(n+segundos)/60;
+                resto=(n+segundos)%60;
+                segundos=resto;
+                if (minutos+cociente<=59){
+                    minutos=minutos+cociente;
+                } else {
+                    cociente2=(minutos+cociente)/60;
+                    minutos=(minutos+cociente)%60;
+                    horas=horas+cociente2;
+                }
+            }
+            System.out.println(horas+" horas");
+            System.out.println(minutos+" minutos");
+            System.out.println(segundos+" segundos");
+            n = sc.nextInt();
+        }
+    }
+}
